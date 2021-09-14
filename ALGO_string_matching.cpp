f7582a91fc2135ec5cbccbba48e5145a0b380417
RubyHuynh <hthn.oo@gmail.com>
2021-09-14 22:18:27 +0700

    Update ALGO_string_matching.cpp
    kmp

--- ALGO_string_matching.cpp
+++ ALGO_string_matching.cpp
@@ -1,12 +1,57 @@
 /******************************************************************************
- 12/09/2021
+ 12/09/2021, 14/09/2021
 
 *******************************************************************************/
 #include <stdio.h>
 #include <iostream>
 #include <ostream>
+#include <vector>
 
+int kmpPreprocess(std::string& patt, std::vector<int>& shiftArr) {
+    int m = patt.size();
+    int i = 0, j = -1;
+    shiftArr[0] = -1;
+    printf("%s\n", __func__);
+    while (i < m) {
+        while (j >= 0 && patt.at(j) != patt.at(j)) {
+            j = shiftArr[j];
+        }
+        i++;
+        j++;
+        shiftArr[i] = j;
+    }
+    
+}
 
+/**/
+int kmp(std::string& name, std::string& patt) {
+    if (name.size() == 0 || patt.size() == 0) {
+        return -1;
+    }
+    
+    int i = 0, j = 0;
+    int n = name.size();
+    int m = patt.size();
+    printf("\n%s %s(%d) vs %s(%d)\n", __func__, name.c_str(), n, patt.c_str(), m);
+    std::vector<int> shiftArr(m+1);
+    kmpPreprocess(patt, shiftArr);
+    while (i < n) {
+        printf("\tstart at %s...\n", name.substr(i).c_str());
+        if (j >= 0 && name.at(i) != patt.at(j)) {
+            printf("\t\t not match %s(%d-->%d)...\n", patt.substr(j).c_str(), j, shiftArr[j]);
+            j = shiftArr[j];
+        }
+        i++;
+        j++;
+        if (j == m) {
+            printf("\nFinished\n");
+            return i - m;
+        }
+    }
+    return -1;
+}
+
+/**/
 int robinKarp(std::string& name, std::string& patt) {
     if (name.size() == 0 || patt.size() == 0) {
         return -1;
@@ -94,6 +139,8 @@ int main()
     printf("RS=%d\n", bruceForceSearch(name, patt));
     
     printf("RS=%d\n", robinKarp(name, patt));
+    
+    printf("RS=%d\n", kmp(name, patt));
     return 0;
 }
 /*
@@ -131,6 +178,33 @@ robinKarp Nakiri Alice(12) vs lice(4)
                 comparing i == i?
                 comparing c == c?
                 comparing e == e?
+
+Finished
+RS=8
+
+
+kmp Nakiri Alice(12) vs lice(4)
+kmpPreprocess
+        start at Nakiri Alice...
+                 not match lice(0-->-1)...
+        start at akiri Alice...
+                 not match lice(0-->-1)...
+        start at kiri Alice...
+                 not match lice(0-->-1)...
+        start at iri Alice...
+                 not match lice(0-->-1)...
+        start at ri Alice...
+                 not match lice(0-->-1)...
+        start at i Alice...
+                 not match lice(0-->-1)...
+        start at  Alice...
+                 not match lice(0-->-1)...
+        start at Alice...
+                 not match lice(0-->-1)...
+        start at lice...
+        start at ice...
+        start at ce...
+        start at e...
 
 Finished
 RS=8
