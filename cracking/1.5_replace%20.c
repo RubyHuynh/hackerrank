432de00abd7671014667bc2202990c39586ab7fb
Ruby Huynh <hthn.oo@gmail.com>
2021-11-02 08:10:32 +0000

    crack d3 string: 1.5 replace%20 no space

--- cracking/1.5_replace%20.c
+++ cracking/1.5_replace%20.c
@@ -5,8 +5,9 @@
 #include<sys/time.h>
 #include "../util.h"
 
-/* 01 ways:
+/* 02 ways:
  * O(n) time + O(n+k) space 
+ * O(n) time, given that input array already has enough n+k space
  * */
 char* func1(char* ss) {
 	int i = 0;
@@ -24,7 +25,6 @@ char* func1(char* ss) {
 		i++;
 	}
 	s = (char*) malloc(sizeof(char) * (n + cnt*2));
-	
 	for (i = 0; i < n; i++) {
 		if (ss[i] == ' ') {
 			s[j++] = '%';
@@ -43,12 +43,51 @@ _done:
 	return s;
 }
 
+char* func2(char* ss) {
+	int i = 0;
+	int j = 0;
+	int n = strlen(ss);
+	int cnt = 0;
+	TM_INIT
+	TM_START(__func__)
+	printf("====%s<%s>\n", __func__, ss);
+	while (i < n) {
+		if (ss[i] == ' ') {
+			++cnt;
+		}
+		i++;
+	}
+	if (cnt == 0) goto _done;
+	cnt = n + cnt*2;
+	ss[cnt--] = '\0';
+	for (i = n-1; i >=0; i--) {
+		if (ss[i] == ' ') {
+			ss[cnt--] = '0';
+			ss[cnt--] = '2';
+			ss[cnt--] = '%';
+		}
+		else {
+			ss[cnt--] = ss[i];
+		}
+		printf("\t\t %d %s\n", i, ss);
+	}
+_done:
+	TM_END(__func__)
+	printf("\t%s\n", ss);
+	return ss;
+}
+
 int main() {
 	int i = 0;
 	char* arr[] = {"asd fgh   k", "  a n d s s dewc", "sabd", "a", " skfjoiwqewj  " };
+	char* tmp;
 	while (i < sizeof(arr)/sizeof(arr[i])) {
 		printf("\n");
 		func1(arr[i]);
+		tmp = (char*)malloc(sizeof(char)*100);
+		sprintf(tmp, "%s", arr[i]);
+		func2(tmp);
+		free(tmp);
 		i++;
 	} 
 }
