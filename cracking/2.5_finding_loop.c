f71eb98289ca52a7563ee5708c3e2217abee488f
Ruby Huynh <hthn.oo@gmail.com>
2021-11-07 14:57:21 +0000

    crack d7 linked list: find loop (richtig)

--- cracking/2.5_finding_loop.c
+++ cracking/2.5_finding_loop.c
@@ -6,54 +6,66 @@
  * O(n^2) time no space
  * */
 List* func1(List* list) {
-	List* cur = NULL, *ret = NULL;
-	List** checked = NULL;
+	List* p1 = NULL, *ret = NULL;
+	List* p2 = NULL, *tmp = NULL;
 	int i = 0, cnt = 0;
 	TM_INIT
 	TM_START(__func__)
 	printf("====%s\n", __func__);	
-	cur = list;
-	while (cur) {
-		++i;
-		cur = cur->next;
-	}
-	checked = (List**) calloc(1, sizeof(List*)*i);
-	i = 0;
-	cur = list;
-	while (cur) {
-		i = 0;
-		DEBUG(printf("\t cur=%p(%d) \n", cur, cur->val);)
-		while (i < cnt && checked[i] && checked[i]->val != cur->val) {
-			DEBUG(printf("\t chechedk[%d]=%p vs cur=%p \n", i, checked[i], cur);)
-			i++;
+	p1 = list;
+	p2 = list;
+	while (p2->next) {
+		p1 = p1->next;
+		p2 = p2->next->next;
+		
+		if (p1 == p2) {
+			break;
 		}
-		if (checked[i] && checked[i]->val == cur->val) {
-			ret = cur;
-			printf("\t got %p = %d\n", cur, cur->val);
-			goto _done;
-		}
-		checked[cnt++] = cur;
-		cur = cur->next;
 	}
+	if (!p2->next) {
+		goto _done;
+	}
+
+	p1 = list;
+	while (p1 != p2) {
+		p1 = p1->next;
+		p2 = p2->next;
+	}
+	ret = p2;
 			
+	printf("\t %p(%d)\n", ret, ret->val);
 _done:
 	TM_END(__func__)
-	free(checked);
 	printf("\t %s\n", ret ? "FOUND" : "NOT FOUND");
 	return ret;
 }
 
+
+
 int main() {
 	int i = 0, val = 0;
-	List* list = NULL, *list2 = NULL;
+	List* list = NULL, *list2 = NULL, *tmp = NULL;
 	srand(time(NULL));
 	while (i++ < 45) {
 		val = rand() % (300 + 1 - 0) + 0;
 		add(&list, val);
-		add(&list2, val);
 	}
-	add(&list, 1); add(&list,2);add(&list,3), add(&list,1); add(&list,4);
-	dump(list);
+	List* lo = (List*) malloc(sizeof(List));
+	lo->val = 1;
+	lo->next = (List*) malloc(sizeof(List));
+	lo->next->val = 2;
+	lo->next->next =  (List*) malloc(sizeof(List));
+	lo->next->next->val = 3;
+	lo->next->next->next =  (List*) malloc(sizeof(List));
+	lo->next->next->next->val = 4;
+	lo->next->next->next->next =  (List*) malloc(sizeof(List));
+	lo->next->next->next->next->val = 5;
+	tmp = list->next;
+	list->next = lo;
+	lo->next->next->next = lo;/*tmp;*/
+
+	//add(&list,4);
+	//dump(list);
 	func1(list);
 	return 0;
 }
