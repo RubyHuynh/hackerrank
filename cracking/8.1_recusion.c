6f1cd984ff772b65dc21314c77c58027043ab1f5
Ruby Huynh <hthn.oo@gmail.com>
2021-12-08 14:38:23 +0000

    crack d22, 8.1 fibo + 8.2 MxN paths count

--- cracking/8.1_recusion.c
+++ cracking/8.1_recusion.c
@@ -22,6 +22,57 @@ int fibo1(int n) {
 	return b;
 }
 
+/*8.2 paths in NxN */
+int numberOfPaths(int m, int n) {
+	if (m == 1 || n == 1) return 1;
+
+	return numberOfPaths(m, n-1) + numberOfPaths(m-1, n);
+}
+int numberOfPaths1(int m, int n) {
+	int i = 0, j = 0;
+	int count[m][n];
+
+	for (; i < m; i++) {
+		count[i][0] = 1;
+	}
+
+
+	for (; j < n; j++) {
+		count[0][j] = 1;
+	}
+
+	for (i = 1; i < m; i++) {
+		for (j = 1; j < n; j++) {
+			count[i][j] = count[i-1][j] + count[i][j-1];
+		}
+	}
+	return count[m-1][n-1];
+}
+
+int numberOfPaths2(int m, int n) {
+	int dp[20] = { 1 };
+	int i = 0, j = 1;
+	dp[0] = 1;
+	           
+	for (; i < m; i++) {
+	    	for (; j < n; j++) {
+			dp[j] += dp[j - 1];
+		}
+	}
+	return dp[n-1];
+}
+
+int numberOfPaths3(int m, int n) {
+	    // We have to calculate m+n-2 C n-1 here
+	    //     // which will be (m+n-2)! / (n-1)! (m-1)!
+	int path = 1;
+	int i = n;
+	for (; i < (m + n - 1); i++) {
+		path *= i;
+		path /= (i - n + 1);
+	}
+	return path;
+}
 
 int main() {
 	TM_INIT
@@ -31,4 +82,23 @@ int main() {
 	TM_START("iterative")
 	printf("fibo(40) = %d\n", fibo1(40));
 	TM_STOP("iterative");
+
+
+	TM_START("number of paths")
+	printf("7x4 = %d\n", numberOfPaths(7, 14));
+	TM_STOP("number of paths");
+
+
+	TM_START("number of paths, DP")
+	printf("7x4 = %d\n", numberOfPaths1(7, 14));
+	TM_STOP("number of paths, DP")
+
+
+	TM_START("number of paths, DP2")
+	printf("7x4 = %d\n", numberOfPaths2(7, 14));
+	TM_STOP("number of paths, DP2")
+
+	TM_START("number of paths, Math")
+	printf("7x4 = %d\n", numberOfPaths3(7, 14));
+	TM_STOP("number of paths, Math")
 }
