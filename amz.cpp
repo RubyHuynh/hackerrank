04ac483af3d5936b43be3676955d4ff388f2d1a9
RubyHuynh <hthn.oo@gmail.com>
2022-06-26 14:39:23 +0000

    AMZ 3. Merge two sorted linked lists

--- amz.cpp
+++ amz.cpp
@@ -1,9 +1,43 @@
 #include<iostream>
 #include<ostream>
 #include<vector>
+#include<list>
 #include<unordered_map>
 
 using namespace std;
+
+
+// runtime O(m+n), space O(m+n)
+list<int>* merge2Lists(std::list<int>* l1, list<int>* l2) {
+	if (!l1->size()) return l2;
+	if (!l2->size()) return l1;
+	auto i1 = l1->begin();
+	auto i2 = l2->begin();
+	list<int>* rs = new list<int>();
+
+	while (i1 != l1->end() || i2 != l2->end()) {
+		if (i1 != l1->end() && i2 != l2->end()) {
+			if (*i1 < *i2) {
+				rs->push_back(*i1);
+				i1++;
+			} 
+			else {
+				rs->push_back(*i2);
+				i2++;
+			}
+		}
+		else if (i1 != l1->end()) {
+			rs->push_back(*i1);
+			i1++;
+		}
+		else {
+			rs->push_back(*i2);
+			i2++;
+		}
+	}
+	return rs;
+}
+
 
 bool sumTwo(vector<int> &arr, int n) {
 	unordered_map<int, int> map;
@@ -51,5 +85,16 @@ int main() {
 	cout << "findMissingNo(8) " << findMissingNo(arr, 8) << "\n";
 	cout << "findMissingNo1(8) " << findMissingNo1(arr, 8) << "\n";
 	cout << "sumTwo(9) " << sumTwo(arr, 9) << "\n";
+
+	list<int> l1{4,8,15,19};
+	list<int> l2{7, 9, 10, 16};
+	cout << "merge2Lists\n";
+	auto merged = merge2Lists(&l1, &l2);
+	auto it = merged->begin();
+	while (it != merged->end()) {
+		cout << *it << ", ";
+		it++;
+	}
+
 	return 0;
 }
