4e1b838a165cc62a585273ab33996c63aee84b5c
RubyHuynh <hthn.oo@gmail.com>
2022-06-27 14:55:32 +0000

    4. Copy linked list with arbitrary pointer

--- amz.cpp
+++ amz.cpp
@@ -7,6 +7,63 @@
 using namespace std;
 
 
+struct Node {
+	int val;
+	Node *tail;
+	Node *next;
+	Node *arbitrary;
+	Node(int v) : val(v), next(NULL), arbitrary(NULL), tail (NULL) {};
+	Node* add(int v) {
+		Node *item = new Node(v);
+		item->arbitrary = this->next;
+		item->next = this;
+		return item;
+	}
+
+	Node* add(Node* n) {
+		Node* item = new Node(n->val);
+		item->arbitrary = n->arbitrary;
+		item->next = this;
+		return item;
+	}
+};
+
+// TODO reverse
+// O(n)
+Node* deepCopy(Node *list) {
+	Node *rs = NULL;
+	Node *tmp = list;
+	Node *newItem;
+	int i = 0;
+	unordered_map<Node*, Node*> pos;
+
+	while (tmp) {
+		cout << tmp->val << " (" << tmp->arbitrary  << ") ";
+		if (!rs) {
+			newItem = rs = new Node(tmp->val);
+		}
+		else {
+			newItem = rs->add(tmp);
+			rs = newItem;
+		}
+		newItem->arbitrary = tmp->arbitrary;
+		pos[tmp] = newItem;
+		tmp = tmp->next;
+	}
+
+	// remapping arbitrary
+	tmp = rs;
+	while (tmp) {
+		if (tmp->arbitrary) {
+			tmp->arbitrary = pos[tmp->arbitrary];
+		}
+		tmp = tmp->next;
+	}
+	return rs;
+}
+
+
+
 // runtime O(m+n), space O(m+n)
 list<int>* merge2Lists(std::list<int>* l1, list<int>* l2) {
 	if (!l1->size()) return l2;
@@ -95,6 +152,17 @@ int main() {
 		cout << *it << ", ";
 		it++;
 	}
-
+	
+	//
+	cout << "deepCopy arbitrary list\n";
+	Node* node1 = new Node(1);
+	node1 = node1->add(2);
+	node1 = node1->add(3);
+	Node* cloned = deepCopy(node1);
+	cout << "\n";
+	while (cloned) {
+		cout << cloned->val << " (" << cloned->arbitrary  << ") ";
+		cloned = cloned->next;
+	}
 	return 0;
 }
