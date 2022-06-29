355baae1b1b3af53fc21b044954542fa3bce533d
RubyHuynh <hthn.oo@gmail.com>
2022-06-29 02:27:17 +0000

    AMZ bubble sort

--- sorting.cpp
+++ sorting.cpp
@@ -72,9 +72,9 @@ void dump(T &arr) {
 void bubble(std::vector<int> &arr) {
 	std::cout << __func__ << "\n";
 	for (int i = 0; i < arr.size(); i++) {
-		for (int j = i; j < arr.size(); j++) {
-			if (arr[i] > arr[j]) {
-				std::swap(arr[i], arr[j]);
+		for (int j = i; j < arr.size() - i - 1; j++) {
+			if (arr[j] > arr[j+1]) {
+				std::swap(arr[j], arr[j+1]);
 			}
 		}
 	}
