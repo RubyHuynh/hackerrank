273c0b0c7ccb27fc6f5c6a298b4054524229b0ca
RubyHuynh <hthn.oo@gmail.com>
2022-06-30 02:26:30 +0000

    ANKI merge sort

--- sorting.cpp
+++ sorting.cpp
@@ -68,6 +68,54 @@ void dump(T &arr) {
 		std::cout << *it << ", ";
 	}
 }
+
+
+void mergeArr(std::vector<int> &arr, int l, int m, int r) {
+	int i, j, k;
+	int n1 = m - l +1; // first part
+	int n2 = r -m; // second part
+	
+	int L[n1], R[n2];
+
+	// temp arrays
+	for (int i  = 0; i < n1; i++) {
+		L[i] = arr[l+i];
+	}
+	for (int i = 0; i < n2; i++) {
+		R[i] = arr[m + 1 + i];
+	}
+	
+
+	i = 0; j = 0; k = l;
+	while (i < n1 && j < n2) {
+		if (L[i] <= R[j]) {
+			arr[k++] = L[i++];
+		}
+		else {
+			arr[k++] = R[j++];
+		}
+	}
+	
+	while (i < n1) {
+		arr[k++] = L[i++];
+	}
+
+	while (j < n2) {
+		arr[k++] = R[j++];
+	}
+}
+
+// O(nlogn)
+void merge(std::vector<int> &arr, int l, int r) {
+	if (l < r) {
+		int m = l + (r-l)/2;
+		merge(arr, l, m);
+		merge(arr, m + 1, r);
+		
+		mergeArr(arr, l, m, r);
+	}
+}
+
 // O(n^2), O(1)
 void bubble(std::vector<int> &arr) {
 	std::cout << __func__ << "\n";
@@ -95,5 +143,12 @@ int main() {
 	dump(arr);
 	bubble(arr);
 	dump(arr);
+
+	
+	std::cout << "\nmerge sort\n";
+	std::vector<int> arr2{100,2,4,21,4,12,31, 5};
+	dump(arr2);
+	merge(arr2, 0, arr2.size() -1);
+	dump(arr2);
 	return 0;
 }
