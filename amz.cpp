c4fac10b5dad3d8a5411452a62f8f286277da32e
RubyHuynh <hthn.oo@gmail.com>
2022-07-02 14:30:49 +0000

    9. How many ways can you make change with coins and a total amount

--- amz.cpp
+++ amz.cpp
@@ -6,8 +6,99 @@
 #include<queue>
 #include<limits.h>
 #include<algorithm>
+#include<string.h>
 
 using namespace std;
+
+
+// 9.
+int coint3( int S[], int m, int n )         
+{         
+ // table[i] will be storing the number of solutions for         
+ // value i. We need n+1 rows as the table is constructed         
+ // in bottom up manner using the base case (n = 0)         
+ int table[n+1];         
+ // Initialize all table values as 0         
+ memset(table, 0, sizeof(table));         
+ // Base case (If given value is 0)         
+ table[0] = 1;         
+ // Pick all coins one by one and update the table[] values         
+ // after the index greater than or equal to the value of the         
+ // picked coin         
+ for(int i=0; i<m; i++)         
+ for(int j=S[i]; j<=n; j++)         
+ table[j] += table[j-S[i]];         
+ return table[n];         
+}    
+
+int coin2(vector<int>& a, int v, int n,
+               vector<vector<int> >& dp)
+{
+    if (v == 0)
+        return dp[n][v] = 1;
+    if (n == 0)
+        return 0;
+    if (dp[n][v] != -1)
+        return dp[n][v];
+    if (a[n - 1] <= v) {
+        // Either Pick this coin or not
+        return dp[n][v] = coin2(a, v - a[n - 1], n, dp)
+                          + coin2(a, v, n - 1, dp);
+    }
+    else // We have no option but to leave this coin
+        return dp[n][v] = coin2(a, v, n - 1, dp);
+}
+
+int coin1( int S[], int m, int n )
+{
+    int i, j, x, y;
+ 
+    int table[n + 1][m];
+     
+    for (i = 0; i < m; i++)
+        table[0][i] = 1;
+ 
+    for (i = 1; i < n + 1; i++)
+    {
+        for (j = 0; j < m; j++)
+        {
+            // Count of solutions including S[j]
+            x = (i-S[j] >= 0) ? table[i - S[j]][j] : 0;
+ 
+            // Count of solutions excluding S[j]
+            y = (j >= 1) ? table[i][j - 1] : 0;
+ 
+            // total count
+            table[i][j] = x + y;
+        }
+    }
+    return table[n][m - 1];
+}
+
+int coin(int S[], int m, int n)
+{
+     
+    // If n is 0 then there is 1 solution
+    // (do not include any coin)
+    if (n == 0)
+        return 1;
+     
+    // If n is less than 0 then no
+    // solution exists
+    if (n < 0)
+        return 0;
+ 
+    // If there are no coins and n
+    // is greater than 0, then no
+    // solution exist
+    if (m <= 0 && n >= 1)
+        return 0;
+ 
+    // count is sum of solutions (i)
+    // including S[m-1] (ii) excluding S[m-1]
+    return coin(S, m - 1, n) +
+           coin(S, m, n - S[m - 1]);
+}
 
 // 8. Reverse Words in a Sentence
 // O(n), O(n)
@@ -409,5 +500,18 @@ int main() {
 	//8.
 	std::string s("Stuying with the cat above my head, literally");
 	reverse(s);
+
+	// 9. 
+	{
+		int i, j;
+                int arr[] = { 1, 2, 3 };
+                int m = sizeof(arr) / sizeof(arr[0]);
+     
+	        cout << "\n9. How many ways can you make change with coins and a total amount\n";
+	        cout << "\n\t" << coin(arr, m, 4);
+
+	}
+
+     	cout<< "\n";
 	return 0;
 }
