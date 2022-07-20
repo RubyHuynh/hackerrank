285303e01594b46a1770e477e7cc0ee6fda515a7
RubyHuynh <hthn.oo@gmail.com>
2022-07-20 19:10:32 +0700

    41. First Missing Positive

--- hard_leet.cpp
+++ hard_leet.cpp
@@ -1,4 +1,62 @@
 
+void swapp(int *a, int *b) {
+    int c= *a;
+    *a = *b;
+    *b = c;
+}
+// 41. First Missing Positive
+// https://leetcode.com/problems/first-missing-positive
+class Solution {
+public:
+    /*We only need to keep track of which ones of the first n positive integers occur in the array. To do this with no extra space, we can treat the array like a boolean array, where sign(arr[i]) indicates whether the i-th positive integer is present or not. First, we replace all non-positive values with n + 1, since we'll only use these indices as storage space. Then, for every one of the first n positive numbers in the array, we turn the value at their corresponding index negative. Finally, all we need to do is check for the first positive value, which will give us the first missing positive. If we find no such index, then all natural numbers up to n are present, so we return n + 1.
+    
+    in short
+we replace the negative values with n+1 values so that they will be out of the range
+we make the index negative if we found that value in the array
+we identify the index which is not negative and it will be the missing number
+
+Runtime: 254 ms, faster than 52.43% of C++ online submissions for First Missing Positive.
+Memory Usage: 82.9 MB, less than 77.72% of C++ online submissions for First Missing Positive.
+Next challenges:
+    */
+    int firstMissingPositive(vector<int>& nums) {
+        int n = nums.size();
+        // for (int i = 0; i < n; i++) if (nums[i] <= 0) nums[i] = n + 1;
+        // for (int i = 0; i < n; i++) if (abs(nums[i]) <= n && nums[abs(nums[i]) - 1] > 0) nums[abs(nums[i]) - 1] *= -1;
+        // for (int i = 0; i < n; i++) if (nums[i] > 0) return i + 1;
+        // return n + 1;
+        
+        
+        /*
+        For anyone who doubts on the complexity of this algorithm: just consider position with A[i] = i+1 as a CORRECT SLOT.
+
+        CORRECT SLOT will never be changed: for CORRECT SLOT, A[A[i] - 1] always equals to A[i], vice versa (1)
+        For each swap, the number of CORRECT SLOT increases by at least 1: Position A[A[i] - 1] = A[i] becomes a CORRECT SLOT after swap, and according to (1), this must be a new CORRECT SLOT
+        The maximum of CORRECT SLOT <= N
+        Therefore, the complexity is O(N)
+
+        We visit each number once, and each number will be put in its right place at most once, so it is O(n) + O(n). Although there are two nesting of cyclic (for and while), but its time complexity is still O(n).
+        
+        Runtime: 350 ms, faster than 24.62% of C++ online submissions for First Missing Positive.
+Memory Usage: 83 MB, less than 41.35% of C++ online submissions for First Missing Positive.
+        */
+        for(int i = 0; i < n; ++ i)
+            while(nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i])
+                swapp(&nums[i], &nums[nums[i] - 1]);
+        
+        for(int i = 0; i < n; ++ i)
+            if(nums[i] != i + 1)
+                return i + 1;
+        
+        return n + 1;
+    }
+    
+    
+    
+};
+
+
+
 // 23. Merge k Sorted Lists
 //https://leetcode.com/problems/merge-k-sorted-lists
 /**
