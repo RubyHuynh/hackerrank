c48ecba359aa13778098669b58197752009ea41e
RubyHuynh <hthn.oo@gmail.com>
2022-07-22 19:50:09 +0700

    86. Partition List

--- med_leet.cpp
+++ med_leet.cpp
@@ -4,6 +4,38 @@
 #include<limits.h>
 using namespace std;
 
+//86. Partition List
+class Solution {
+public:
+    
+    ListNode* partition(ListNode* head, int x) {
+        
+        ListNode* small = new ListNode(-1);
+        ListNode* large = new ListNode(-1);
+        ListNode* small_head = small;
+        ListNode* large_head = large;
+        
+        while (head){
+            if (head->val < x){
+                small->next = head;
+                small = small -> next;
+                head = head -> next;
+                small->next = NULL;
+            }
+            else{
+                large->next = head;
+                large = large -> next;
+                head = head -> next;
+                large -> next = NULL;
+            }
+        }
+        small -> next = large_head -> next;
+
+        return small_head -> next;
+    }
+};
+
+
 //93. Restore IP Addresses
 //https://leetcode.com/problems/restore-ip-addresses/submissions/
 vector<string> restoreIpAddresses(string s) {
