e6a688f633281336da07576430f6571de4674c71
RubyHuynh <hthn.oo@gmail.com>
2022-07-26 21:30:47 +0700

    44. Wildcard Matching

--- hard_leet.cpp
+++ hard_leet.cpp
@@ -1,4 +1,50 @@
+//44. Wildcard Matching
+class Solution {
+public:
+//     bool isMatch(string s, string p) {
+//         vector<vector<bool>> dp(s.size() + 1, vector(p.size() + 1, false));
+//         dp[0][0] = true;
+//         for (int j = 0; j < p.size() && p[j] == '*'; ++j) {
+//             dp[0][j + 1] = true;
+//         }
+        
+//         for (int i = 1; i <= s.size(); ++i) {
+//             for (int j = 1; j <= p.size(); ++j) {
+//                 if (p[j - 1] == '*') {
+//                     dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+//                 } else {
+//                     dp[i][j] = (s[i - 1] == p[j - 1] || p[j - 1] == '?') && dp[i - 1][j - 1];
+//                 }
+//             }
+//         }
 
+//         return dp[s.size()][p.size()];
+//     }
+    
+     bool isMatch(string s, string p) {
+        vector<bool> dp(p.size() + 1, false);
+        dp[0] = true;
+        for (int j = 0; j < p.size() && p[j] == '*'; ++j) {
+            dp[j + 1] = true;
+        }
+        
+        for (int i = 1; i <= s.size(); ++i) {
+            dp[0] = false;
+            bool neighborLastRow = i == 1;
+            for (int j = 1; j <= p.size(); ++j) {
+                bool currLastRow = dp[j];
+                if (p[j - 1] == '*') {
+                    dp[j] = currLastRow || dp[j - 1];
+                } else {
+                    dp[j] = (s[i - 1] == p[j - 1] || p[j - 1] == '?') && neighborLastRow;
+                }
+                neighborLastRow = currLastRow;
+            }
+        }
+
+        return dp[p.size()];
+    }
+};
 //37. Sudoku Solver
 class Solution {
 public:
