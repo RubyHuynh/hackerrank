9939433d56f2b5345d6b768232f68b161c0d69e0
RubyHuynh <hthn.oo@gmail.com>
2022-07-29 21:26:25 +0700

    121. Best Time to Buy and Sell Stock
    Sliding windows

--- easy_leet.cpp
+++ easy_leet.cpp
@@ -1,3 +1,34 @@
+//121. Best Time to Buy and Sell Stock
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        //pointers only move right
+        int l=0;//left pointer------- logic: get the least value possible
+        int r=0;//right pointer------- logic: traverse the array and update profit, if you get something less than left pointer, then update left pointer, 
+        
+        int profit=0;
+        
+        //we move r while price[l] is lower than price[r]
+        while(++r<prices.size()){
+            
+            if(prices[r]>prices[l]){
+            
+                profit=max(profit,prices[r]-prices[l]);//this is update to the max of current value of p and the profit if the share bought at l(the lowest so far) is sold at r
+            
+            }else if(prices[r]<prices[l]){
+                //right pointer points to a value lower than left pointer
+                
+                l=r;// since l can be at most r, it can never cross r
+                //keeping new lower value of l will be more profitable 
+                //for the future higher values
+            }
+        }
+        
+        return profit;
+    }
+};
+
+
 //125. Valid Palindrome
 class Solution {
 public:
