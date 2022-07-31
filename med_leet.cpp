d6ce2668f2b0de8cd7d5b84f1cd4a1a5056229d5
RubyHuynh <hthn.oo@gmail.com>
2022-07-31 20:36:39 +0700

    209. Minimum Size Subarray Sum
    basic sliding windows

--- med_leet.cpp
+++ med_leet.cpp
@@ -4,6 +4,39 @@
 #include<limits.h>
 using namespace std;
 
+// 209. Minimum Size Subarray Sum
+class Solution {
+public:
+    
+    int minSubArrayLen(int s, vector<int>& nums) {
+        int l = 0, r = 0, n = nums.size(), sum = 0, len = INT_MAX;
+        while (r < n) {
+            sum += nums[r++];
+            while (sum >= s) {
+                len = min(len, r - l);
+                sum -= nums[l++];
+            }
+        }
+        return len == INT_MAX ? 0 : len;
+    }
+        
+//     int minSubArrayLen(int target, vector<int>& nums) {
+//         int l = 0, r = 0, tmp = nums[l];
+//         int rs = INT_MAX;
+//         for (int i = 0; i < nums.size(); i++) {
+//             if (r!=0) {
+//                 tmp += nums[r];
+//             }
+//             if (tmp >= target) {
+//                 rs = min(rs, r - l -1);
+//                 tmp -= nums[l];
+//                 l++;
+//             }
+//             r++;
+//         }
+//         return rs;
+//     }
+};
 
 // 424. Longest Repeating Character Replacement
 /*
