c053608a42dcfdede2d1ae889cb10d63975aebec
RubyHuynh <hthn.oo@gmail.com>
2022-08-02 10:14:24 +0700

    198. House Robber
    Nick White

--- med_leet.cpp
+++ med_leet.cpp
@@ -4,8 +4,18 @@
 #include<limits.h>
 using namespace std;
 
-
-
+// 198. House Robber
+ int rob(vector<int>& nums) {
+        if (nums.size() == 0) return 0;
+        vector<int> dp(nums.size()+1);
+        dp[0] = 0;
+        dp[1] = nums[0];
+        for (int i = 1; i < nums.size(); i++) {
+            dp[i+1] = max(dp[i], dp[i-1]+nums[i]);
+        }
+        return dp[nums.size()];
+        
+    }
 class Solution {
 public:
 //     int rob(vector<int>& nums) {
