b53eeab13e52fb0c34537eb250d399390a0f03c3
RubyHuynh <hthn.oo@gmail.com>
2022-08-07 22:02:12 +0700

    234. Palindrome Linked List
    3 steps:
    1. fast + 2, slow + 1.
    2. Reverse slow at the middle
    3. Compare one by one.

--- easy_leet.cpp
+++ easy_leet.cpp
@@ -1,3 +1,55 @@
+// 234. Palindrome Linked List
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+        
+        while (fast && fast->next) {
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+        
+        slow = reverse(slow);
+        fast = head;
+        
+        while (slow) {
+            if (slow->val != fast->val) {
+                return false;
+            }
+            slow = slow->next;
+            fast = fast->next;
+        }
+        return true;
+    }
+private:
+    ListNode* reverse(ListNode* head) {
+        ListNode* prev = NULL;
+        
+        while (head) {
+            ListNode* next = head->next;
+            head->next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+};
+/*Runtime: 357 ms, faster than 55.14% of C++ online submissions for Palindrome Linked List.
+Memory Usage: 114 MB, less than 85.80% of C++ online submissions for Palindrome Linked List.*/
+
+
+
 //167. Two Sum II - Input Array Is Sorted
 class Solution {
 public:
