19f47119d694c81c86f1f286ce41ca23f77a237f
RubyHuynh <hthn.oo@gmail.com>
2022-08-08 22:12:10 +0700

    354. Russian Doll Envelopes
    phu`

--- hard_leet.cpp
+++ hard_leet.cpp
@@ -1,3 +1,40 @@
+//354. Russian Doll Envelopes
+class Solution {
+public:
+    static bool comp(vector<int> &a, vector<int> &b){
+        if(a[0]==b[0]){
+            return a[1]>b[1];
+        }
+        return a[0]<b[0];
+    }
+        
+    int maxEnvelopes(vector<vector<int>>& envelopes) {
+        sort(envelopes.begin(), envelopes.end(), comp);
+        // sort(envelopes.begin(), envelopes.end(), [](const auto a, const auto b) {
+        //     if(a[0]==b[0]) {
+        //         return a[1]>b[1];
+        //     }
+        //     return a[0]<b[0];
+        // });
+        int i, j, n=envelopes.size();
+        vector<int>lis;
+        
+        for(i=0; i<n; i++){
+            auto it = lower_bound(lis.begin(), lis.end(), envelopes[i][1]);
+            if(it==lis.end()){lis.push_back(envelopes[i][1]);}
+            else{
+                *it = envelopes[i][1];
+            }
+        }
+        return lis.size();
+    }
+};
+/*
+Runtime: 673 ms, faster than 38.83% of C++ online submissions for Russian Doll Envelopes.
+Memory Usage: 77.6 MB, less than 66.73% of C++ online submissions for Russian Doll Envelopes.*/
+
+
+
 // 132. Palindrome Partitioning II
 // class Solution {
 //     public:
