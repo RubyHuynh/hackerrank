ecdbc870bdc625566830255b41ee03a982a0cec2
RubyHuynh <hthn.oo@gmail.com>
2022-08-10 21:38:20 +0700

    72. Edit Distance
    Dp bottomup

--- hard_leet.cpp
+++ hard_leet.cpp
@@ -1,3 +1,42 @@
+//72. Edit Distance
+class Solution {
+public:
+int minDistance(string word1, string word2) {
+
+    int n=word1.length(),m=word2.length();
+    int dp[n+1][m+1];
+    memset(dp,-1,sizeof(dp));
+    //word2 string (target) is empty
+    for(int i=0;i<=n;i++)
+    {
+        dp[i][0]=i; // j is zero , target is empty , total operations to delete
+    }
+    // word1 is empty (source) , insert all element into source from target , operation will be size of target
+    for(int j=0;j<=m;j++)
+    {
+        dp[0][j]=j;
+    }
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=m;j++)
+        {
+            
+            if(word1[i-1]==word2[j-1])
+            {
+                dp[i][j]=dp[i-1][j-1];
+            }
+            else //if(word1[i-1]!=word2[j-1])
+            {
+                dp[i][j]=min(dp[i-1][j-1] , min(dp[i][j-1] , dp[i-1][j])) +1; // replace , insert , delete
+            }
+        }
+    }
+    return dp[n][m];
+}
+};
+
+
+
 //354. Russian Doll Envelopes
 class Solution {
 public:
