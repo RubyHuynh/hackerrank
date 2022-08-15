ca8cac07b708c31fd3b1872ade4b42be3d32e080
RubyHuynh <hthn.oo@gmail.com>
2022-08-15 21:36:24 +0700

    55. Jump Game
    2,0,0 true?

--- easy_leet.cpp
+++ easy_leet.cpp
@@ -1,3 +1,16 @@
+//55. Jump Game
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        int goal = n-1, i;
+        for (int i = n; i--;)
+            if (i + nums[i] >= goal)
+                goal = i;
+        return !goal;
+    }
+};
+
 //9. Palindrome Number
 // class Solution {
 // public:
