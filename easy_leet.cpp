49bc4b72793d5fd9af34307663f7d705764d28c8
RubyHuynh <hthn.oo@gmail.com>
2022-08-16 09:28:00 +0700

    101. Symmetric Tree

--- easy_leet.cpp
+++ easy_leet.cpp
@@ -1,3 +1,32 @@
+//101. Symmetric Tree
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        return isSym(root, root);
+    }
+    
+    bool isSym(TreeNode* l, TreeNode* r) {
+        if (!l && !r) return true;
+        if (!l || !r) return false;
+        
+        return l->val == r->val && isSym(l->left, r->right) && isSym(l->right, r->left);
+    }
+};
+/*Runtime: 9 ms, faster than 47.74% of C++ online submissions for Symmetric Tree.
+Memory Usage: 16.5 MB, less than 18.26% of C++ online submissions for Symmetric Tree.*/
+
+
 //55. Jump Game
 class Solution {
 public:
