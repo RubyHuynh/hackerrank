347baaeea6b534cdc228261a1c81d05de7e1bf08
RubyHuynh <hthn.oo@gmail.com>
2022-08-17 20:59:46 +0700

    695. Max Area of Island
    Same a show many islands

--- med_leet.cpp
+++ med_leet.cpp
@@ -5,6 +5,40 @@
 using namespace std;
 
 
+//695. Max Area of Island
+int maxAreaOfIsland(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        
+        int result = 0;
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    result = max(result, dfs(grid, i, j, m, n));
+                }
+            }
+        }
+        
+        return result;
+    }
+private:
+    int dfs(vector<vector<int>>& grid, int i, int j, int m, int n) {
+        if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == 0) {
+            return 0;
+        }
+        grid[i][j] = 0;
+        
+        return 1 + dfs(grid, i - 1, j, m, n) + dfs(grid, i + 1, j, m, n)
+            + dfs(grid, i, j - 1, m, n) + dfs(grid, i, j + 1, m, n);
+    }
+/*Runtime: 20 ms, faster than 85.47% of C++ online submissions for Max Area of Island.
+Memory Usage: 23.2 MB, less than 86.95% of C++ online submissions for Max Area of Island.*/
+
+
+
+
+
 //15. 3Sum
 class Solution {
 public:
