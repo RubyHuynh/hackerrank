adc0c8449b7b9f58a2fff457b52ce12c74b95075
RubyHuynh <hthn.oo@gmail.com>
2022-09-21 09:38:38 +0700

    39. Combination Sum
    backtracking, erase and start index

--- med_leet.cpp
+++ med_leet.cpp
@@ -4,6 +4,52 @@
 #include<limits.h>
 using namespace std;
 
+
+//39. Combination Sum
+class Solution {
+public:
+    
+    void Sum(vector<int>& candidates, int target, vector<vector<int> >& res, vector<int>& r, int i)
+    {
+        
+        if(target == 0)
+        {
+            // if we get exact answer
+            res.push_back(r);
+            return;
+        }
+        
+        while(i <  candidates.size() && target - candidates[i] >= 0)
+        {
+            // Till every element in the array starting
+            // from i which can contribute to the target
+            r.push_back(candidates[i]);// add them to vector
+            
+            // recur for next numbers
+            Sum(candidates,target - candidates[i],res,r,i);
+            ++i;
+            
+            // Remove number from vector (backtracking)
+            r.pop_back();
+        }
+    }
+    
+     
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        sort(candidates.begin(),candidates.end()); // sort candidates array
+        
+        // remove duplicates
+        candidates.erase(unique(candidates.begin(),candidates.end()),candidates.end());
+        
+        vector<int> r;
+        vector<vector<int> > res;
+        
+        Sum(candidates,target,res,r,0);
+        
+        return res;
+    }
+};
+
 //208. Implement Trie (Prefix Tree)
 class Trie {
     Trie* next[26] = {};
