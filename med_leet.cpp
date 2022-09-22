d7250d1ec0af21ca13e09d7b1798e1835b4b1aa3
RubyHuynh <hthn.oo@gmail.com>
2022-09-22 09:26:54 +0700

    5. Longest Palindromic Substring

--- med_leet.cpp
+++ med_leet.cpp
@@ -4,6 +4,74 @@
 #include<limits.h>
 using namespace std;
 
+//5. Longest Palindromic Substring
+class Solution {
+public:
+     string longestPalindrome1(string s) 
+    {   
+        int len = s.size();
+        int dp[len][len];
+        memset(dp,0,sizeof(dp));
+        int end=1;
+        int start=0;
+
+        for(int i=0;i<len;i++)
+        {
+            dp[i][i] = 1;
+        }
+        for(int i=0;i<len-1;i++)
+        {
+            if(s[i]==s[i+1])
+            { dp[i][i+1]=1;start=i;end=2;}
+        }
+
+        for(int j=2;j<len;j++)
+        {
+            for(int i=0;i< len-j;i++)
+            {  
+                int left=i; //start point
+                int right = i+j;  //ending point
+
+                if(dp[left+1][right-1]==1 && s[left]==s[right]) 
+                {
+                    dp[left][right]=1; start=i; end=j+1; 
+                }        
+            }
+        }
+       return s.substr(start, end);
+    }
+    
+    string longestPalindrome(string s) {
+        if(s.size()==0) return "";
+        int i = 0, j = 0;
+        int n = s.size();
+        //initialize P[n][n], we only need half of P, and initialize it like: (e.g. : s="abbc")
+        bool P[n][n];
+        for(int x = 0;x<n;x++){
+            P[x][x]=true;
+            if(x==n-1) break;
+            P[x][x+1] = (s[x]==s[x+1]);
+        }
+        //dp 
+        for(int i = n-3; i>=0; --i){
+            for(int j = i+2;j<n; ++j){
+                P[i][j] = (P[i+1][j-1] && s[i]==s[j]);     
+            }
+        }
+        //get maxstr result
+        int max = 0;
+        string maxstr = "";
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                if(P[i][j]==true and j-i+1>max){
+                    max = j-i+1;
+                    maxstr = s.substr(i,j-i+1);
+                }
+            }
+        }
+        return maxstr;
+    }
+};
 
 //39. Combination Sum
 class Solution {
