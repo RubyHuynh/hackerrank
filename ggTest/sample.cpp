4ecedeb588089a22251bcefdf2f5c83eb602d3a5
RubyHuynh <hthn.oo@gmail.com>
2023-03-06 09:19:59 +0000

    add google test

--- ggTest/sample.cpp
+++ ggTest/sample.cpp
@@ -2,51 +2,49 @@
 #include <ostream>
 #include "gtest/gtest.h"
 using namespace std;
+
+
 // The function under test
 int strStr(string haystack, string needle) {
-int szH = haystack.size();
-int szN = needle.size();
-int j = 0;
+	int szH = haystack.size();
+	int szN = needle.size();
+	int j = 0;
 
-if (szH == 0)
-return 0;
-for (int i = 0; i < szH; i++) {
-if (haystack[i] == needle[j]) {
-j++;
-}
-else {
-i -= j;
-j = 0;
-}
-if (j == szN) {
-return i - j + 1;
-}
-}
-return -1;
+	if (szH == 0)
+		return 0;
+	for (int i = 0; i < szH; i++) {
+		if (haystack[i] == needle[j]) {
+			j++;
+		}
+		else {
+			i -= j;
+			j = 0;
+		}
+		if (j == szN) {
+			return i - j + 1;
+		}
+	}
+	return -1;
 }
 
-// The test case class
 class StrStrTest : public ::testing::Test {
-protected:
-// You can define any objects you need here
+	protected:
 };
 
-// The first test
 TEST_F(StrStrTest, EmptyNeedle) {
-// Check if the function returns zero when needle is empty
-EXPECT_EQ(0, strStr("hello", ""));
+	EXPECT_EQ(-1, strStr("hello", ""));
 }
 
-// The second test
-TEST_F(StrStrTest, NeedleFound) {
-// Check if the function returns the correct index when needle is found
-EXPECT_EQ(2, strStr("hello", "ll"));
+TEST_F(StrStrTest, NeedleNotFoundd) {
+	EXPECT_EQ(-1, strStr("hello", "helloooo"));
 }
 
-// The third test
 TEST_F(StrStrTest, NeedleNotFound) {
-// Check if the function returns -1 when needle is not found
-EXPECT_EQ(-1, strStr("hello", "world"));
+	EXPECT_EQ(-1, strStr("hello", "world"));
+}
+
+TEST_F(StrStrTest, NeedleFound) {
+	EXPECT_EQ(5, strStr("helloworld", "world"));
 }
 
 int main(int argc, char** argv) {
