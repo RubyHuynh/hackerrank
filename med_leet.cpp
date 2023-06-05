dd30bdcb3824e21612b83a7a062d986befd5a068
RubyHuynh <hthn.oo@gmail.com>
2023-06-05 21:40:58 +0700

    1232. Check If It Is a Straight Line

--- med_leet.cpp
+++ med_leet.cpp
@@ -4,6 +4,44 @@
 #include<limits.h>
 using namespace std;
 
+
+
+//1232. Check If It Is a Straight Line
+class Solution {
+public:
+    bool checkStraightLine(vector<vector<int>>& coordinates) {
+        int dx = coordinates[1][0] - coordinates[0][0];
+        int dy = coordinates[1][1] - coordinates[0][1];
+
+        /*
+                dy1/dx1 = dy2/dx2
+            <-> dy1*dx2 = dy2*dx1
+            
+        */
+        for (int i = 2; i < coordinates.size(); i++) {
+            if (dx * (coordinates[i][1] - coordinates[0][1]) 
+                != dy * (coordinates[i][0] - coordinates[0][0])) {
+                    return false;
+            }
+        }
+        return true;
+    }
+};
+
+
+class Solution:
+    def checkStraightLine(self, coordinates: List[List[int]]) -> bool:
+        dx = coordinates[1][0] - coordinates[0][0]
+        dy = coordinates[1][1] - coordinates[0][1]
+
+        for i in coordinates : 
+            if (dx * (i[1] - coordinates[0][1])) != (dy * (i[0] - coordinates[0][0])) :
+                return False
+        return True
+	
+	
+	
+
 //547. Number of Provinces
 class Solution {
 public:
