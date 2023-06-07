33de1b6ec056397ca88ff7259c241680d7e87b2a
RubyHuynh <hthn.oo@gmail.com>
2023-06-07 21:20:05 +0700

    1318. Minimum Flips to Make a OR b Equal to c

--- med_leet.cpp
+++ med_leet.cpp
@@ -5,8 +5,45 @@
 using namespace std;
 
 
-	
-	
+//1318. Minimum Flips to Make a OR b Equal to c
+class Solution {
+public:
+    int minFlips(int a, int b, int c) {
+        int ret = 0;
+
+        while (a !=0 | b != 0 | c != 0) {
+            if ((c & 1) == 1) {
+                if ((a & 1) == 0 && (b & 1) == 0 ) {
+                    ret++;
+                }
+            }
+            else {
+                ret += (a & 1) + (b & 1);
+            }
+            a >>= 1;
+            b >>= 1;
+            c >>= 1;
+        }
+        return ret;
+    }
+};
+class Solution:
+    def minFlips(self, a: int, b: int, c: int) -> int:
+        ret = 0
+
+        while a != 0 or b != 0 or c != 0 :
+            if c & 1 == 1 :
+                if a & 1 == 0 and b & 1 == 0 :
+                    ret += 1
+            else :
+                ret += (a & 1) + (b & 1)
+            a >>= 1
+            b >>= 1
+            c >>= 1
+
+        return ret
+
+
 
 //547. Number of Provinces
 class Solution {
