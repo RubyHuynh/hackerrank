3f143b6039be8e1bfc67cf1104c7812744c72df2
RubyHuynh <hthn.oo@gmail.com>
2023-06-11 23:01:57 +0700

    1146. Snapshot Array

--- med_leet.cpp
+++ med_leet.cpp
@@ -5,6 +5,41 @@
 using namespace std;
 
 
+//1146. Snapshot Array
+class SnapshotArray {
+    int snapId;
+    vector<vector<pair<int, int>>> history;
+public:
+    SnapshotArray(int length) {
+        this->snapId = 0;
+        this->history.resize(length);
+        for (int i = 0; i < length; i++) {
+            history[i].push_back(make_pair(0,0));
+        }
+    }
+    
+    void set(int index, int val) {
+        history[index].push_back(make_pair(snapId, val));
+    }
+    
+    int snap() {
+        return snapId++;
+    }
+    
+    int get(int index, int snap_id) {
+        auto it = upper_bound(history[index].begin(), history[index].end(), make_pair(snap_id, INT_MAX));
+        return prev(it)->second;
+    }
+};
+
+/**
+ * Your SnapshotArray object will be instantiated and called as such:
+ * SnapshotArray* obj = new SnapshotArray(length);
+ * obj->set(index,val);
+ * int param_2 = obj->snap();
+ * int param_3 = obj->get(index,snap_id);
+ */
+
 //1802. Maximum Value at a Given Index in a Bounded Array
 class Solution {
 public:
