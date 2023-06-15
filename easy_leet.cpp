3ceffc1da824b33d85769a03b65f684a4b84d761
RubyHuynh <hthn.oo@gmail.com>
2023-06-15 22:01:07 +0700

    530. minimum different in BST

--- easy_leet.cpp
+++ easy_leet.cpp
@@ -1,3 +1,81 @@
+//530. Minimum Absolute Difference in BST
+class Solution {
+public:
+    int minDifference = INT_MAX;
+    // Initially, it will be null.
+    TreeNode* prevNode;
+        
+    void inorderTraversal(TreeNode* node) {
+        if (node == NULL) {
+            return;
+        }
+        
+        inorderTraversal(node->left);
+        // Find the difference with the previous value if it is there.
+        if (prevNode != NULL) {
+            minDifference = min(minDifference, node->val - prevNode->val);
+        }
+        prevNode = node;
+        inorderTraversal(node->right);
+    }
+    
+    int getMinimumDifference(TreeNode* root) {
+        inorderTraversal(root);
+        return minDifference;
+    }
+};
+class Solution2 {
+public:
+    // List to store the tree nodes in the inorder traversal.
+    vector<int> inorderNodes;
+
+    void inorderTraversal(TreeNode* node) {
+        if (node == NULL) {
+            return;
+        }
+
+        inorderTraversal(node->left);
+        // Store the nodes in the list.
+        inorderNodes.push_back(node->val);
+        inorderTraversal(node->right);
+    }
+
+    int getMinimumDifference(TreeNode* root) {
+        inorderTraversal(root);
+
+        int minDifference = INT_MAX;
+        // Find the diff between every two consecutive values in the list.
+        for (int i = 1; i < inorderNodes.size(); i++) {
+            minDifference = min(minDifference, inorderNodes[i] - inorderNodes[i - 1]);
+        }
+
+        return minDifference;
+    }
+};
+class Solution1 {
+public:
+    vector<int> arr;
+
+    void dfs(TreeNode* node) {
+        if (!node) return;
+        arr.push_back(node->val);
+        dfs(node->left);
+        dfs(node->right);
+    }
+
+    int getMinimumDifference(TreeNode* root) {
+        dfs(root);
+
+        sort(arr.begin(), arr.end());
+        int ret = INT_MAX;
+
+        for (int i = 1; i < arr.size(); i++) {
+            ret = min(arr[i] - arr[i-1], ret);
+        }
+        return ret;
+    }
+};
+
 //228. Summary Ranges
 class Solution {
 public:
