603840b22ff0e3dadaad49b21ffa508e7b1a1b79
RubyHuynh <hthn.oo@gmail.com>
2023-06-25 15:47:36 +0700

    1575. Count All Possible Routes

--- hard_leet.cpp
+++ hard_leet.cpp
@@ -1,3 +1,28 @@
+//1575. Count All Possible Routes
+class Solution {
+public:
+    int dp[101][201];
+    int mod = 1e9 + 7;
+    int countRoutes(vector<int>& locations, int start, int finish, int fuel) {
+        memset(dp, -1, sizeof(dp));
+        return rec(locations, start, finish, fuel);
+    }
+
+    int rec(vector<int>& locations, int idx, int finish, int fuel) {
+        if (fuel < 0) return 0;
+        long long ret = 0;
+
+        if (dp[idx][fuel] != -1) return dp[idx][fuel] % mod;
+
+        for (int i = 0; i < locations.size(); i++) {
+            if (i == idx) continue;
+            ret = (ret % mod + rec(locations, i, finish, fuel - abs(locations[i] - locations[idx]))%mod) % mod;
+        }
+        if (idx == finish) return dp[idx][fuel] = (1 + ret) % mod;
+        return dp[idx][fuel] = ret % mod;
+    }
+};
+
 //956. Tallest Billboard
 class Solution {
 public:
