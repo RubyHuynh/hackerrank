8784457f4c8ed5f416ad71571ed373082cdcc1b2
RubyHuynh <hthn.oo@gmail.com>
2023-07-17 22:40:37 +0700

    445. Add Two Numbers II

--- med_leet.cpp
+++ med_leet.cpp
@@ -4,7 +4,88 @@
 #include<limits.h>
 using namespace std;
 
+//445. Add Two Numbers II
 
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* temp;
+        while (head) {
+            // Keep the next node.
+             temp = head->next;
+            // reverse the link
+            head->next = prev;
+            // Update the previous node and the current node.
+            prev = head;
+            head = temp;
+        }
+        return prev;
+    }
+
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* r1 = reverseList(l1);
+        ListNode* r2 = reverseList(l2);
+
+        int totalSum = 0;
+        int carry = 0;
+        ListNode* ans = new ListNode();
+        while (r1 || r2) {
+            if (r1) {
+                totalSum += r1->val;
+                r1 = r1->next;
+            }
+            if (r2) {
+                totalSum += r2->val;
+                r2 = r2->next;
+            }
+
+            ans->val = totalSum % 10;
+            carry = totalSum / 10;
+            ListNode* head = new ListNode(carry);
+            head->next = ans;
+            ans = head;
+            totalSum = carry;
+        }
+
+        return carry == 0 ? ans->next : ans;
+    }
+};
+class Solution1 {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        stack<int> s1, s2;
+        while (l1) {
+            s1.push(l1->val);
+            l1 = l1->next;
+        }
+        while (l2) {
+            s2.push(l2->val);
+            l2 = l2->next;
+        }
+
+        int totalSum = 0, carry = 0;
+        ListNode* ret = new ListNode();
+        while (!s1.empty() || !s2.empty()) {
+            if (!s1.empty()) {
+                totalSum += s1.top();
+                s1.pop();
+            }
+            if (!s2.empty()) {
+                totalSum += s2.top();
+                s2.pop();
+            }
+
+            ret->val = totalSum % 10;
+            carry = totalSum / 10;
+            ListNode* next = new ListNode(carry);
+            next->next = ret;
+            ret = next;
+            totalSum = carry;
+        }
+        return carry == 0 ? ret->next : ret;
+    }
+};
 //1218. Longest Arithmetic Subsequence of Given Difference
 
 class Solution {
