da75273e1caaa521effadeb3dffe5dfa2cd0879a
RubyHuynh <hthn.oo@gmail.com>
2023-08-01 21:24:53 +0700

    77. Combinations

--- med_leet.cpp
+++ med_leet.cpp
@@ -3,6 +3,33 @@
 #include<string.h>
 #include<limits.h>
 using namespace std;
+
+
+//77. Combinations
+
+class Solution {
+public:
+    vector<vector<int>> arr;
+    vector<int> nums;
+    void bt(vector<int> nums, vector<vector<int>>& arr, int n, int k, int pos, int cur) {
+        if (pos == k) {
+            arr.push_back(nums);
+            return;
+        }
+        for (int i = cur; i <= n - k + pos + 1; i++) {
+            nums[pos] = i;
+            bt(nums, arr, n, k, pos+1, i+1);
+        }
+    }
+
+
+    vector<vector<int>> combine(int n, int k) {
+        nums = vector<int>(k);
+        bt(nums, arr, n, k, 0, 1);
+        return arr;
+    }
+};
+
 //712. Minimum ASCII Delete Sum for Two Strings
 
 class Solution {
