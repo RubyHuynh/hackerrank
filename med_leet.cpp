02e5817979da053956280cf3fb690a3eb1e1c616
RubyHuynh <hthn.oo@gmail.com>
2023-10-03 16:31:49 +0700

    Spiral Matrix

--- med_leet.cpp
+++ med_leet.cpp
@@ -4,6 +4,33 @@
 #include<limits.h>
 using namespace std;
 
+
+//54. Spiral Matrix
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        int r_min = 0, c_min = 0;
+        int r_max = matrix.size() - 1, c_max = matrix[0].size() - 1;
+        double all_items = matrix.size()*matrix[0].size();
+        vector<int> ret;
+
+        for (double i = 0, r = 0, c = 0; i < all_items; i++) {
+            ret.push_back(matrix[r][c]);
+            if      (r == r_min && c != c_max) c++;
+            else if (r != r_max && c == c_max) r++;
+            else if (r == r_max && c != c_min) c--;
+            else if (r != r_min && c == c_min) r--;
+
+            if (r == r_min && c == c_min) {
+                r++, c++;
+                r_min++, c_min++;
+                r_max--, c_max--;
+            }
+        }
+        return ret;
+    }
+};
+
 //1498. Number of Subsequences That Satisfy the Given Sum Condition
 class Solution {
 public:
