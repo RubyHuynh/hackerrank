c0410e1a74e4264e2e72375ae3fb5c9d749a3ebf
RubyHuynh <hthn.oo@gmail.com>
2023-10-05 13:32:14 +0700

    1035. Uncrossed Lines

--- med_leet.cpp
+++ med_leet.cpp
@@ -4,6 +4,63 @@
 #include<limits.h>
 using namespace std;
 
+//1035. Uncrossed Lines
+class Solution {
+public:
+    int solve(int i, int j, vector<int>& nums1, vector<int>& nums2, vector<vector<int>> &memo) {
+        if (i <= 0 || j <= 0) return 0;
+        if (memo[i][j] != -1) return memo[i][j];
+        if (nums1[i - 1] == nums2[j - 1]) {
+            memo[i][j] = 1 + solve(i-1, j-1, nums1, nums2, memo);
+        }
+        else {
+            memo[i][j] = max(solve(i, j-1, nums1, nums2, memo), solve(i-1, j, nums1, nums2, memo));
+        }
+        return memo[i][j];
+    }
+    int maxUncrossedLines1(vector<int>& nums1, vector<int>& nums2) {
+        int n1 = nums1.size();
+        int n2 = nums2.size();
+        vector<vector<int>> memo(n1+1, vector<int>(n2+1, -1));
+        return solve(n1, n2, nums1, nums2, memo);
+    }
+    int maxUncrossedLines2(vector<int>& nums1, vector<int>& nums2) {
+        int n1 = nums1.size();
+        int n2 = nums2.size();
+        vector<vector<int>> memo(n1+1, vector<int>(n2+1, 0));
+        
+        for (int i = 1; i <= n1; i++) {
+            for (int j = 1; j <= n2; j++) {
+                if (nums1[i-1] == nums2[j-1]) {
+                    memo[i][j] = 1 + memo[i-1][j-1];
+                }
+                else {
+                    memo[i][j] = max(memo[i][j-1], memo[i-1][j]);
+                }
+            }
+        }
+        return memo[n1][n2];
+    }
+    int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
+        int n1 = nums1.size();
+        int n2 = nums2.size();
+
+        vector<int> dp(n2 + 1), dpPrev(n2 + 1);
+
+        for (int i = 1; i <= n1; i++) {
+            for (int j = 1; j <= n2; j++) {
+                if (nums1[i - 1] == nums2[j - 1]) {
+                    dp[j] = 1 + dpPrev[j - 1];
+                } else {
+                    dp[j] = max(dp[j - 1], dpPrev[j]);
+                }
+            }
+            dpPrev = dp;
+        }
+
+        return dp[n2];
+    }
+};
 
 //54. Spiral Matrix
 class Solution {
