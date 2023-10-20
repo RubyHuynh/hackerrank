4312b3fa0a6d477b4eb9c0a43c9bf434345df439
RubyHuynh <hthn.oo@gmail.com>
2023-10-20 13:04:48 +0700

    2542. Maximum Subsequence Score

--- med_leet.cpp
+++ med_leet.cpp
@@ -4,7 +4,33 @@
 #include<limits.h>
 using namespace std;
 
+//2542. Maximum Subsequence Score
+class Solution {
+public:
+    long long maxScore(vector<int>& nums1, vector<int>& nums2, int k) {
+        int n = nums1.size();
+        vector<pair<int, int>> v;
+        for (int i = 0 ; i < n; i++) {
+            v.push_back({nums2[i], nums1[i]});
+        }
 
+        sort(v.begin(), v.end(), greater<>());
+        priority_queue<int, vector<int>, greater<int>> pq;
+        long long sum =0, ret = 0;
+        for (int i = 0; i < n; i++) {
+            pq.push(v[i].second);
+            sum+= v[i].second;
+            if (pq.size() > k) {
+                sum -= pq.top();
+                pq.pop();
+            }
+            if (pq.size() == k) {
+                ret = max(ret, sum * v[i].first);
+            }
+        }
+        return ret;
+    }
+};
 
 //934. Shortest Bridge
 class Solution {
