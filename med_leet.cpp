8b830657578f628da4727e05800085841356e6a3
RubyHuynh <hthn.oo@gmail.com>
2023-11-01 13:00:25 +0700

    1140. Stone game II

--- med_leet.cpp
+++ med_leet.cpp
@@ -5,6 +5,48 @@
 using namespace std;
 
 
+//1140. Stone game II
+class Solution {
+public:
+    int xstoneGameII(vector<int>& piles) {
+        int n = piles.size();
+        function<int(int, int, int)> f = [&] (int p, int i, int m) -> int {
+            if (i == n) return 0;
+            int res = p == 1 ? 1000000 : -1, s = 0;
+            for (int x = 1; x <= min(2*m, n-i); x++) {
+                s += piles[i+x-1];
+                if (p == 0) res = max(res, s + f (1, i+x, max(m,x)));
+                else        res = min(res, f(0, i+x, max(m,x)));
+            }
+            return res;
+        };
+        return f(0, 0, 1);
+    }
+
+    int stoneGameII(vector<int>& piles) {
+        int n = piles.size();    
+        vector dp(2, vector(n+1, vector<int>(n+1, -1)));
+
+        function<int(int, int, int)> f = [&] (int p, int i, int m) -> int {
+            if (i == n) return 0;
+
+            if (dp[p][i][m] != -1) return dp[p][i][m];
+            int res = p == 1 ? INT_MAX : INT_MIN, s = 0;
+            for (int x = 1; x <= min(2*m, n - i); x++) {
+                s += piles[i+x-1];
+                if (p) {
+                    res = min(res, f(0, i+x, max(m, x)));
+                }
+                else {
+                    res = max(res, s + f(1, i+x, max(m, x)));
+                }
+            }
+            return dp[p][i][m] = res;
+        };
+        return f(0, 0, 1);
+    }
+
+};
 
 //877. Stone Game
 class Solution {
