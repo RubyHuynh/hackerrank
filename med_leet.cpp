8e287b3e57d0134d1ec2054ad4b571d41874319c
RubyHuynh <hthn.oo@gmail.com>
2023-11-10 13:03:28 +0700

    43. Multiply Strings

--- med_leet.cpp
+++ med_leet.cpp
@@ -5,6 +5,44 @@
 using namespace std;
 
 
+
+//43. Multiply Strings
+class Solution {
+public:
+    string multiply(string num1, string num2) {
+        if (num1 == "0" || num2 == "0") return "0";
+        string s1 = "";
+        string s2 = "";
+
+        if (s1[0] == '-') {
+            s1 = "-"; num1 = num1.substr(1);
+        }
+        if (s2[0] == '-') {
+            s2 = "-"; num2 = num2.substr(1);
+        }
+
+        if (s1 == "-" && s2 == "-") {
+            s1 = s2 = "";
+        }
+
+        int n = num1.size();
+        int m = num2.size();
+        string ret(n+m, '0');
+
+        for (int i = n-1; i >= 0; i--) {
+            for (int j = m-1; j >= 0; j--) {
+                int num = (num1[i] - '0') * (num2[j] - '0') + (ret[i+j+1] - '0'); // ret w carry
+                ret[i+j+1] = num %10 + '0';
+                ret[i+j] += num/10;
+            }
+        }
+
+        int i = 0;
+        while (i < ret.size() && ret[i] == '0') i++;
+        return s1 + s2 + ret.substr(i);
+    }
+};
+
 //47. Permutations II
 class Solution {
 public:
