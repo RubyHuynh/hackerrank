17ca698fc791cfdf8289220ec8ca886160415d4b
RubyHuynh <hthn.oo@gmail.com>
2023-11-13 13:31:33 +0700

    35. Search Insert Position

--- easy_leet.cpp
+++ easy_leet.cpp
@@ -1,3 +1,24 @@
+//35. Search Insert Position
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int l = 0; 
+        int r = nums.size();
+        int mid; 
+
+        if (target > nums[r-1]) return r;
+
+        while (l <= r) {
+            mid = (r+l)/2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[mid] > target) r = mid - 1;
+            else l = mid + 1;
+        }
+        return l;
+    }
+};
 
 //121. Best Time to Buy and Sell Stock
 class Solution {
