e91277a6443a3523085056c428ec977e90d7139e
RubyHuynh <hthn.oo@gmail.com>
2023-12-08 09:28:47 +0700

    36. Valid Sudoku

--- med_leet.cpp
+++ med_leet.cpp
@@ -4,6 +4,63 @@
 #include<limits.h>
 using namespace std;
 
+//36. Valid Sudoku
+class Solution1{
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        unordered_map<int,unordered_map<char,int>> board_map;
+        for(auto i = 0; i<9; i++){
+            for(auto j = 0; j<9; j++){
+                char cell = board[i][j];
+                if(cell != '.'){
+                    if(board_map[i][cell]++ > 0) return false;
+                    if(board_map[j+9][cell]++ > 0) return false;
+                    if(board_map[27 + (i/3)*3 +j/3][cell]++ > 0) return false;
+                    }
+            }
+        }
+        return true;
+    }
+};
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        unordered_map<char,int> eachBox;
+        unordered_map<char,int> row;
+        unordered_map<char,int> column ;
+        int j= 0 ;
+        int i=0 ;
+        for( i =0 ; i < 9 ;i++){
+            for( j=0 ; j< 9 ;j++){
+                if(board[i][j] != '.'){
+                    row[board[i][j]]++;
+                }
+                if(board[j][i]!='.'){
+                    column[board[j][i]]++;
+                }
+                if(row[board[i][j]]>1) return false;
+                if(column[board[j][i]]>1) return false ;
+            }
+            row.clear();
+            column.clear();
+        }
+        for(i=0 ; i < 9 ;i+=3){
+            for(j=0 ; j <9 ;j+=3){
+                eachBox.clear();
+                for(int x = i ; x < i+3 ; x++){
+                    for(int y = j ; y < j+3; y++){
+                        if(board[x][y]!='.'){
+                            eachBox[board[x][y]]++;
+                        }
+                        if(eachBox[board[x][y]]>1) return false ;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+};
 
 //11. Container With Most Water
 class Solution {
