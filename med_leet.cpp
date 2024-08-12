ef4a885e200aa04a8d2950b435575a83c5ad6fa8
RubyHuynh <hthn.oo@gmail.com>
2024-08-12 20:46:06 +0700

    143. Reorder List
    kind of divide and conquer, find half, reverse the latter part, merge

--- med_leet.cpp
+++ med_leet.cpp
@@ -4,6 +4,53 @@
 #include<limits.h>
 using namespace std;
 
+
+//143. Reorder List
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    void reorderList(ListNode* head) {
+        if (!head) return ;
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        ListNode* prev = NULL;
+        ListNode* cur = slow;
+        ListNode* next = NULL;
+        while (cur) {
+            next = cur->next;
+            cur->next = prev;
+            prev = cur;
+            cur = next;
+        }
+
+        ListNode* first = head;
+        ListNode* second = prev;
+        ListNode* tmp = head;
+        while (second->next) {
+            tmp = tmp->next;
+            first->next = second;
+            second = second->next;
+            first->next->next = tmp;
+            first = first->next->next;
+        }
+        return ;
+    }
+};
+
 //24. Swap Nodes in Pairs
 /**
  * Definition for singly-linked list.
