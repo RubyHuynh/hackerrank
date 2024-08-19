9116751dcf52ff36506405f53c8bbc8638f51bd8
RubyHuynh <hthn.oo@gmail.com>
2024-08-19 21:35:33 +0700

    56. Merge Intervals

--- med_leet.cpp
+++ med_leet.cpp
@@ -3,6 +3,32 @@
 #include<string.h>
 #include<limits.h>
 using namespace std;
+//56. Merge Intervals
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end()
+            , [](const vector <int> &a, const vector<int>&b) {
+            return a[0] < b[0];
+        });
+
+        vector<vector<int>> merged;
+        vector<int> prev = intervals[0];
+
+        for (int i = 1; i < intervals.size(); i++) {
+            vector<int> cur = intervals[i];
+            if (cur[0] <= prev[1]) {
+                prev[1] = max(prev[1], cur[1]);
+            }
+            else {
+                merged.push_back(prev);
+                prev = cur;
+            }
+        }
+        merged.push_back(prev);
+        return merged;
+    }
+};
 
 //16. 3Sum Closest
 class Solution {
