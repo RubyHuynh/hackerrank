9a4f6fbfef224cb39b1a0ed3b6c33710233d6a56
RubyHuynh <hthn.oo@gmail.com>
2024-08-24 21:20:03 +0700

    13 roman to integer

--- easy_leet.cpp
+++ easy_leet.cpp
@@ -1,3 +1,29 @@
+//13. Roman to Integer
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<char, int> vals;
+        vals['I'] = 1;
+        vals['V'] = 5;
+        vals['X'] = 10;
+        vals['L'] = 50;
+        vals['C'] = 100;
+        vals['D'] = 500;
+        vals['M'] = 1000;
+
+        int ret = 0;
+        for (int i = 0 ; i < s.size(); i++) {
+            if (vals[s[i]] < vals[s[i+1]]) {
+                ret -= vals[s[i]];
+            }
+            else {
+                ret += vals[s[i]];
+            }
+        }
+        return ret;
+    }
+};
+
 //58. Length of Last Word
 class Solution {
 public:
