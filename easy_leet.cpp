578d4339f33c22ee5deb2001e8051d70800695e9
RubyHuynh <hthn.oo@gmail.com>
2024-08-29 22:15:58 +0700

    104. Maximum Depth of Binary Tree

--- easy_leet.cpp
+++ easy_leet.cpp
@@ -1,3 +1,14 @@
+//104. Maximum Depth of Binary Tree
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (!root) return 0;
+        int l = maxDepth(root->left);
+        int r = maxDepth(root->right);
+        return max(l,r) + 1;
+    }
+};
+
 //66. Plus One
 class Solution {
 public:
