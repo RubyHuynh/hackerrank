0f90ffb84ee8b5cbb5a4478ffe24b2612e39df1e
RubyHuynh <hthn.oo@gmail.com>
2024-09-04 20:55:55 +0700

    100 is same tree

--- easy_leet.cpp
+++ easy_leet.cpp
@@ -1,3 +1,13 @@
+//100 is same tree
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p == nullptr && q == nullptr) return true;
+        if(p == nullptr || q == nullptr) return false;
+        return p->val == q-> val && isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+    }
+};
+
 //104. Maximum Depth of Binary Tree
 class Solution {
 public:
