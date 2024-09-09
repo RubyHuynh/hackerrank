15d695dd75b54779c523cb391fd63694bf88cabf
RubyHuynh <hthn.oo@gmail.com>
2024-09-09 21:16:29 +0700

    130. Surrounded Regions
    mark border using DFS, flip remain. unmark

--- med_leet.cpp
+++ med_leet.cpp
@@ -4,7 +4,51 @@
 #include<limits.h>
 using namespace std;
 
+//130. Surrounded Regions
 
+class Solution {
+    void dfs(vector<vector<char>>& board, int i, int j) {
+        if (i <0 || i >= board.size() 
+            || j <0 || j >= board[0].size()
+            || board[i][j] != 'O') return;
+        board[i][j] = 'B';
+        // expand 4 directions if possible
+        dfs(board, i - 1, j);
+        dfs(board, i + 1, j);
+        dfs(board, i, j - 1);
+        dfs(board, i, j + 1);
+    }
+public:
+    void solve(vector<vector<char>>& board) {
+        if (board.empty()) return;
+        int r = board.size();
+        int c = board[0].size();
+        // find boundary first and last row/column
+        for (int j = 0; j < c; j++) {
+            if (board[0][j] == 'O') dfs(board, 0, j);
+            if (board[r-1][j] == 'O') dfs(board, r-1, j);
+        }
+        for (int i = 0; i < r; i++) {
+            if (board[i][0] == 'O') dfs(board, i, 0);
+            if (board[i][c-1] == 'O') dfs(board, i, c -1);
+        }
+
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                if (board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                }
+            }
+        }
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                if (board[i][j] == 'B') {
+                    board[i][j] = 'O';
+                }
+            }
+        }
+    }
+};
 //134. Gas Station
 class Solution {
 public:
