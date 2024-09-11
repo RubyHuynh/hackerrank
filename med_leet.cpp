7dd8fc54617270237e5e5172f72e69979ebd1d9c
RubyHuynh <hthn.oo@gmail.com>
2024-09-11 21:36:58 +0700

    97. Interleaving String
    dp

--- med_leet.cpp
+++ med_leet.cpp
@@ -4,8 +4,34 @@
 #include<limits.h>
 using namespace std;
 
-//130. Surrounded Regions
 
+//97. Interleaving String
+class Solution {
+public:
+    bool isInterleave(string s1, string s2, string s3) {
+        int m = s1.length();
+        int n = s2.length();
+        int l = s3.length();
+        if (m + n != l) return false;
+        if (m < n) return isInterleave(s2, s1, s3);
+        vector<bool> dp(n+1, false);
+        dp[0] = true;
+
+        for (int j = 1; j <=n ; j++) {
+            dp[j] = dp[j-1] && s2[j-1] == s3[j-1];
+        }
+        for (int i =1; i <= m; i++) {
+            dp[0] = dp[0] && s1[i-1] == s3[i-1];
+            for (int j = 1; j <=n; j++) {
+                dp[j] = (dp[j] && s1[i-1] == s3[i+j-1]) ||
+                        (dp[j - 1] && s2[j - 1] == s3[i + j - 1]);
+            }
+        }
+        return dp[n];
+    }
+};
+
+//130. Surrounded Regions
 class Solution {
     void dfs(vector<vector<char>>& board, int i, int j) {
         if (i <0 || i >= board.size() 
