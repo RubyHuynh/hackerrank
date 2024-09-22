c24c2bb6d73124fa8b348a3eccfc07f7c48382a4
RubyHuynh <hthn.oo@gmail.com>
2024-09-22 22:09:01 +0700

    88. Merge Sorted Array

--- easy_leet.cpp
+++ easy_leet.cpp
@@ -1,3 +1,29 @@
+//88. Merge Sorted Array
+class Solution {
+public:
+    void merge1(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        for (int i = m, j = 0; j < n; j++) {
+            nums1[i] = nums2[j];
+            i++;
+        }
+        sort(nums1.begin(), nums1.end());
+    }
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = m -1;
+        int j = n -1;
+        int k = m + n -1;
+
+        while (j >= 0) {
+            if (i >=0 && nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            }
+            else {
+                nums1[k--] = nums2[j--];
+            }
+        }
+    }
+};
+
 //100 is same tree
 class Solution {
 public:
