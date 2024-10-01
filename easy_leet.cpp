c0a2e54c32d65bd3f105e1184f59ecbbbc99e47a
RubyHuynh <hthn.oo@gmail.com>
2024-10-01 22:23:44 +0700

    110. Balanced Binary Tree (max diff = 1 only)

--- easy_leet.cpp
+++ easy_leet.cpp
@@ -1,3 +1,20 @@
+//110. Balanced Binary Tree (max diff = 1 only)
+class Solution {
+    int height(TreeNode* root) {
+        if (!root) return 0;
+        int l = height(root->left);
+        int r = height(root->right);
+        if (l == -1 || r == -1 || abs(l-r) > 1) return -1;
+        return max(l, r) + 1;
+    }
+public:
+    bool isBalanced(TreeNode* root) {
+        if (!root) return true;
+        if (height(root) == -1) return false;
+        return true;
+    }
+};
+
 //88. Merge Sorted Array
 class Solution {
 public:
