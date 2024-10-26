583688757ba948aac37b2335a9194c9814df5b4f
RubyHuynh <hthn.oo@gmail.com>
2024-10-26 21:48:38 +0700

    649. Dota2 Senate

--- med_leet.cpp
+++ med_leet.cpp
@@ -4,6 +4,29 @@
 #include<limits.h>
 using namespace std;
 
+//649. Dota2 Senate
+class Solution {
+public:
+    string predictPartyVictory(string senate) {
+        queue<int> rad, dir;
+        int n = senate.length();
+        for (int i = 0; i < n; i++) {
+            if (senate[i] == 'R') rad.push(i);
+            else dir.push(i);
+        }
+        while (!rad.empty() && !dir.empty()) {
+            if (rad.front() < dir.front()) {
+                rad.push(n++);
+            }
+            else {
+                dir.push(n++);
+            }
+            rad.pop();
+            dir.pop();
+        }
+        return rad.empty() ? "Dire" : "Radiant";
+    }
+};
 
 //229. Majority Element II
 class Solution {
