c5515be5e6a70c4d8d831c431cb18d5a6867d174
RubyHuynh <hthn.oo@gmail.com>
2024-11-01 22:46:54 +0700

    405. Convert a Number to Hexadecimal

--- easy_leet.cpp
+++ easy_leet.cpp
@@ -1,3 +1,33 @@
+//405. Convert a Number to Hexadecimal
+class Solution {
+    unordered_map<int, char> mp;
+
+public:
+    string toHex(int num) {
+        if (num == 0) return "0";
+
+        for (int i = 0; i < 10; i++){
+            mp[i] = '0' + i;
+        }
+        for (int i = 10; i < 16; i++){
+            mp[i] = 'a' + (i-10);
+        }
+
+        long long n = num;
+        if (num < 0) {
+            n = ((long) INT_MAX*2) + 2 + num; //?
+        }
+        string ret;
+        while (n) {
+            int dig = n %16;
+            ret += mp[dig];
+            n/= 16;
+        }
+        reverse(ret.begin(), ret.end());
+        return ret;
+    }
+};
+
 //404. Sum of Left Leaves
 class Solution {
     int help(TreeNode* root, bool isLeft) {
