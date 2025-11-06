87669d8ceb6406ddbdac18a95433372ddbde2c97
RubyHuynh <hthn.oo@gmail.com>
2025-11-06 17:21:56 +0700

    twoSum
    Added implementations for twoSum, isAnagram, and hasDuplicate functions.

--- refesh_nov2025.cpp
+++ refesh_nov2025.cpp
@@ -1,4 +1,57 @@
+//easy
+class Solution {
+public:
+    // O(n) O(n)
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ret(2,-1);
+        unordered_map<int, int> m;
 
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+            auto item = m.find(complement);
+            if (item != m.end() && item->second != i) {
+                ret[0] =  item->second;
+                ret[1] = i;
+                return ret;
+            }
+            m.insert({nums[i], i});
+        }
+        return ret;
+    }
+};
+
+
+//easy 
+class Solution {
+public:
+    // O(m+n)
+    bool isAnagram1(string s, string t) {
+        vector<int> m(26,0);
+        if (s.size() != t.size()) return false;
+
+        for (int i = 0; i < s.size(); i++) {
+            m[s[i] - 'a']++;
+        }
+        for (int i = 0; i < t.size(); i++) {
+            m[t[i] - 'a']--;
+        }
+        for (int i = 0; i < m.size(); i++){
+            if (m[i] != 0) return false;
+        }
+
+        return true;
+    }
+
+    // O(nlogn + mlogm)
+    bool isAnagram(string s, string t) {
+        std::sort(s.begin(), s.end());
+        std::sort(t.begin(), t.end());
+        return s == t;
+    }
+};
+
+
+//easy
 class Solution {
 public:
     // O(nlogn)
