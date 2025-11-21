b2982926c446a4568fe2d764c89cf6e6a16cc547
RubyHuynh <hthn.oo@gmail.com>
2025-11-21 17:08:36 +0700

    binary search basic

--- refesh_nov2025.cpp
+++ refesh_nov2025.cpp
@@ -1,3 +1,28 @@
+// 704 easy
+class Solution {
+public:
+    // O(logn)
+    int search(vector<int>& nums, int target) {
+        int l = 0;
+        int r = nums.size() - 1;
+        int m;
+
+        while (l <= r) {
+            m = l + (r-l)/2;
+            if (nums[m] == target) {
+                return m;
+            }
+            else if (nums[m] > target) {
+                r = m - 1;
+            }
+            else {
+                l = m + 1;
+            }
+        }
+        return -1;
+    }
+};
+
 //739.medium
 class Solution {
 public:
